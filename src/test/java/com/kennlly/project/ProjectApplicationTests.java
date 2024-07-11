e56f2ba2a63775783941a1baa4613b728361de4a
Kenny He <kenny.he@global.ntt>
2024-07-10 21:44:19 -0400

    Init

---
+++ src/test/java/com/kennlly/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennlly.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
